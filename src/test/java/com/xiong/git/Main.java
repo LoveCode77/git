db3fd356552fab270510bd30eb0e26c54694073e
xiongdu <xiongdu@qq.com>
2021-09-26 23:56:54 +0800

    second commit

--- src/test/java/com/xiong/git/Main.java
+++ src/test/java/com/xiong/git/Main.java
@@ -3,5 +3,7 @@ package com.xiong.git;
 public class Main{
     public static void main(String[] args) {
         System.out.println("xiongdu shi yi ge good man!");
+        System.out.println("xiongdu shi yi ge good man!");
+
     }
 }
